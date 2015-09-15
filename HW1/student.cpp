4f3c15ceeb9fd75cd39d467aa76464331228ef06
Arsh Chauhan <achauhan@alaska.edu>
2015-09-15 11:08:00 -0800

    Passing all tests

--- HW1/student.cpp
+++ HW1/student.cpp
@@ -2,6 +2,8 @@
 
 #include<string> //for std::string
 using std::string;
+#include<sstream> //for std::ostringstream
+using std::ostringstream;
 
 string Student::getName() const
 {
@@ -24,15 +26,41 @@ void Student::setID(int id)
 }
 
 string Student::toString() const
-{
-	//Dummy return 
-	return "toString called"; 
+{ 
+	 int length=1;
+	 int id=_id;
+	 while(id /= 10)
+		length++;
+	
+	string idStr;
+	
+	
+	 if(length <8)
+	 {
+		 int diff=8-length;
+		 for(int i=0; i<diff; i++)
+		 {
+			 idStr += '0';
+		 }
+		 
+	 }
+	 idStr += std::to_string(_id);
+	 
+	return _name + " (" + idStr + ")"; 
 }
 
 bool operator== (const Student &lhs, const Student &rhs)
 {
-	//Dummy return
-	return false;
+	string name1, name2;
+	int id1,id2;
+	
+	name1=lhs.getName();
+	id1=lhs.getID();
+	
+	name2=rhs.getName();
+	id2=rhs.getID();
+	
+	return ((name1==name2)&&(id1==id2));
 }
 
 bool operator!= (const Student &lhs, const Student &rhs)
