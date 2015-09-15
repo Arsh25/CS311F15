0c111c42b8991c1bf75112522e8992a6a4047598
Arsh Chauhan <achauhan@alaska.edu>
2015-09-15 12:53:57 -0800

    Added comments, invariants and private helper functions to meet class coding standards

--- HW1/student.cpp
+++ HW1/student.cpp
@@ -1,46 +1,74 @@
+/*
+ * student.cpp
+ * Arsh Chauhan
+ * 09/15/2015
+ * Source for class Student
+ */
+
 #include "student.h"
 
-#include<string> //for std::string
-using std::string;
-#include<sstream> //for std::ostringstream
-using std::ostringstream;
+#include<iostream>
+using std::cout;
 
+/* getName
+ * Pre: None
+ * Post: 
+ * 	 Returns a C++ string representing the Student's name
+*/
 string Student::getName() const
 {
 	return _name;
 }
 
+/* getID
+ * Pre: None
+ * Post: 
+ * 	 Returns an int which represents the student's ID number
+*/
 int Student::getID() const
 {
 	return _id;
 }
 
+/* setName
+ * Pre: name is a valid C++ sting
+ * Post: _name == name
+*/
 void Student::setName(string name)
 {
 	_name=name;
 }
 
+/* setID
+ * Pre: id is an unsigned int (Max. 8 digits)
+ * Post: _id == id, if id is positive and at most 8 digits 
+*/
 void Student::setID(int id)
 {
+	if(validID()) // Check if id is >0 and at most 8 digits 
 	_id=id;
+	else
+	cout << "Invalid ID: Must be positive and at most 8 digits";
 }
 
+/* toString
+ * Pre: None
+ * Post:
+ * 	  Return is a string giving the student information as _name (_id), _id is padded
+ *    on the left with zeros
+*/
 string Student::toString() const
 { 
-	 int length=1;
-	 int id=_id;
-	 while(id /= 10)
-		length++;
+	 
 	
-	string idStr;
+	string idStr; // String representation of _id
 	
-	
-	 if(length <8)
+	if(idLen() < 8) //Append zeros if _id < 8 digits 
 	 {
-		 int diff=8-length;
-		 for(int i=0; i<diff; i++)
+		 int diff = 8 - idLen();
+		 for(int i = 0; i < diff; i++)
 		 {
-			 idStr += '0';
+			 idStr += '0'; //Append leading zeros
 		 }
 		 
 	 }
@@ -49,26 +77,46 @@ string Student::toString() const
 	return _name + " (" + idStr + ")"; 
 }
 
+
+/* op== for Student
+ * Pre: None
+ * Post: 
+ * 		Returns true if lhs and rhs represent the same student
+ */
 bool operator== (const Student &lhs, const Student &rhs)
 {
 	string name1, name2;
 	int id1,id2;
 	
-	name1=lhs.getName();
-	id1=lhs.getID();
+	name1 = lhs.getName();
+	id1 = lhs.getID();
 	
-	name2=rhs.getName();
-	id2=rhs.getID();
+	name2 = rhs.getName();
+	id2 = rhs.getID();
 	
-	return ((name1==name2)&&(id1==id2));
+	return ((name1 == name2) && (id1 == id2)); // Student objects are equal if both _id and _name are identical 
 }
 
+
+/* op!= for Student
+ * Pre: None
+ * Post: 
+ * 		Returns true if lhs and rhs do not represent the same student
+ */
 bool operator!= (const Student &lhs, const Student &rhs)
 {
-	return !(lhs==rhs);
+	return !(lhs == rhs);
 }
 
-ostream &operator << (ostream &str, const Student &s)
+
+/* Stream insertion op for Student
+ * Pre: None
+ * Post: 
+ * 		Srting form of Student object ( as returned by .toString()) has
+ * 		been printed to the given stream
+ * 		Returns the given stream
+ */
+ostream &operator<< (ostream &str, const Student &s)
 {
 	str << s.toString();
 	return str;
--- HW1/student.h
+++ HW1/student.h
@@ -2,6 +2,7 @@
  * student.h
  * Arsh Chauhan
  * 09/14/2015
+ * Edited: 09/15/2015
  * Header for class Student
  */
 
@@ -13,14 +14,61 @@ using std::string;
 #include<ostream> //for std::ostream
 using std::ostream;
 
+
+/* class Student
+ * Student is uniquely represented by an  _id (ID number)
+ * Invariants:
+ * 		_name is a valid C++ string
+ * 		_id is an unsigned int (Max 8 digits)
+ */
+ 
 class Student
 {
 	string _name;
 	int _id;
 	
+	
+	/* Helper function validID
+	 * Pre: Non
+	 * Post:
+	 * 	  Returns true is _id is >=0 and max 8 digits long
+	*/
+	bool validID() const
+	{
+		return !(_id < 0 || idLen() > 8);
+	}
+	
+	/* Helper function idLen
+	 * Pre: Non
+	 * Post:
+	 * 	  Returns number of digits in _id
+	*/
+	unsigned int idLen() const
+	{
+		int length = 1;
+		int id=_id;
+		while(id /= 10) // Keep moving the decimal point until division by 10 returns 0
+			length++;
+		return length;
+	}
+	
 	public:
+	
+	/* Default ctor
+	 * Pre: None
+	 * Post: 
+	 * 		_name ==" "
+	 * 		_id == 0
+	*/
 	Student():_name(""),_id(0) // Default ctor
 	{}
+	
+	/* 2-parameter ctor
+	 * Pre: None
+	 * Post:
+	 * 		_name == name
+	 * 		_id == id
+	*/
 	Student(string name, unsigned int id):_name(name),_id(id) // 2 parameter ctor
 	{}
 	
